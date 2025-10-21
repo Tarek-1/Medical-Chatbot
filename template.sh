11f18b9b36fd057613b9db6761e1d284705c690b
Tarek-1 <tarekabozaid@hotmail.com>
2025-10-21 10:25:54 +0300

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p src
+mkdir -p research
+
+touch src/main.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory structure created:"
